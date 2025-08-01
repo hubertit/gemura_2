eba8ff68d04601ecc82875d41fb0f4c1cd35062e
hubertit <hubert@itdevs.rw>
2025-08-01 16:18:52 +0200

    Fix import errors: Update package references from 'finance' to 'gemura'

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:finance/main.dart';
+import 'package:gemura/main.dart';
 
 void main() {
   testWidgets('App loads successfully', (WidgetTester tester) async {
